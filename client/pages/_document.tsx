de65bc44fe6955334591280a9038e9310f2beaf1
AG <knightauguest@gmail.com>
2022-06-23 18:18:35 -0400

    font

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -4,10 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
       <body>
